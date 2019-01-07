a8a87dfd4af90c1de9f33776c791a7032317c367
ms <murderedsappho@mail.ru>
2019-01-07 20:39:38 +0200

    Initial commit

---
+++ DragonWeekEnd/Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.Threading
+open Suave
+
+
+
+[<EntryPoint>]
+let main argv =
+    let cancellationTokenSource = new CancellationTokenSource()
+    let config = { defaultConfig with cancellationToken = cancellationTokenSource.Token }
+    let listening, server = startWebServerAsync defaultConfig (Successful.OK "")
+    Async.Start server
+    Console.ReadKey true
+        |> ignore
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
