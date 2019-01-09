df4cf24545bdc8125455ddda39850c430961f683
Eugeniy Shrubek <Eugeniy_Shrubek@epam.com>
2019-01-09 17:56:14 +0200

    Added base type provider, added scheduler and telegram api operations

--- DragonWeekEnd/Program.fs
+++ DragonWeekEnd/Program.fs
@@ -1,17 +1,152 @@
 ﻿// Learn more about F# at http://fsharp.org
 
+open Newtonsoft.Json
+open Quartz
+open Quartz.Impl
+open System.Net.Http
+
+open System.Text
+open FSharp.Data
 open System
-open System.Threading
-open Suave
+
+let ukCustomers =
+   <@@ 1 + 2 @@>
+
+type Updates = JsonProvider<""" {
+    "ok ": true,
+    "result ": [
+        {
+            "update_id ": 293423948,
+            "message ": {
+                "message_id ": 9,
+                "from ": {
+                    "id ": 436295526,
+                    "is_bot ": false,
+                    "first_name ": "Eyes in the ",
+                    "last_name ": "Box ",
+                    "username ": "eyesInTheBox ",
+                    "language_code ": "en "
+                },
+                "chat ": {
+                    "id ": 436295526,
+                    "first_name ": "Eyes in the ",
+                    "last_name ": "Box ",
+                    "username ": "eyesInTheBox ",
+                    "type ": "private "
+                },
+                "date ": 1547041301,
+                "text ": "  u0430  u0432  u044b  u0430  u0432  u044b "
+            }
+        }
+    ]
+} """>
+
+let schedulerFactory = StdSchedulerFactory()
+let scheduler = 
+    let c = async {
+        let! sc = schedulerFactory.GetScheduler()
+        return sc
+    }
+
+    Async.RunSynchronously c
+
+scheduler.Start() |> ignore
+
+type TelegramUpdateMessage = 
+    {
+        Ok: bool;
+        Result: list<TelegramUpdateResponseItem>;
+    }
+and TelegramUpdateResponseItem =
+    {
+        UpdateId: int;
+        Message: TelegramUpdateMessageItem;
+
+    }
+and TelegramUpdateMessageItem =
+    {
+        [<JsonProperty("message_id")>]
+        MessageId: int;
+        Text: string;
+    }
+
+type SendMessageItem = 
+    {
+        [<JsonProperty("chat_id")>]
+        ChatId: int;
+        [<JsonProperty("text")>]
+        Text: string
+    }
+
+let getTelegramString<'T> method = 
+    async {
+        let client = new HttpClient()
+        let url = sprintf "https://api.telegram.org/bot678792687:AAHa9sbP9zT4hm8x8ZD10u1GzJSf6ZIJiJg/%s" method
+        let! res =  client.GetStringAsync(url)
+        let upd = Updates.Parse(res)
+        query {
+            for msg in upd.Result do
+            where ( msg.Message.Chat.Id = 1)
+            select msg
+        }
+
+
+        return JsonConvert.DeserializeObject<'T>(res)
+    }
+
+Async.RunSynchronously (getTelegramString "getUpdates")
+
+let sendMessage chatId message = 
+    async {
+        let client = new HttpClient()
+        let url = "https://api.telegram.org/bot678792687:AAHa9sbP9zT4hm8x8ZD10u1GzJSf6ZIJiJg/sendMessage"
+        let content = new StringContent(JsonConvert.SerializeObject({ ChatId= chatId; Text=message}), Encoding.UTF8, "application/json")
+        let! res =  client.PostAsync(url, content)
+        return res
+    }
+
+//let r1 = Async.RunSynchronously (sendMessage 436295526 "vava")
+
+type TestJob() =
+    interface IJob with
+        member this.Execute(context: IJobExecutionContext) = 
+            let c = async {
+                let! va = (sendMessage 436295526 "vava")
+                Console.WriteLine(DateTime.Now)
+            }
+            
+            Async.StartAsTask c :> _
+            
+let job =
+    JobBuilder.Create<TestJob>().WithIdentity("j1", "g1").Build()
+           
+let trigger =
+    TriggerBuilder
+        .Create()
+        .WithIdentity("t1", "g1")
+        .WithDailyTimeIntervalSchedule(fun x -> 
+            let timezone = TimeZoneInfo.FindSystemTimeZoneById("FLE Standard Time")
+
+            x.InTimeZone(timezone)
+             .StartingDailyAt(TimeOfDay.HourMinuteAndSecondOfDay(16, 26, 0))
+             .OnEveryDay()
+             .Build() |> ignore)
+            .Build()
 
 
 
 [<EntryPoint>]
 let main argv =
-    let cancellationTokenSource = new CancellationTokenSource()
-    let config = { defaultConfig with cancellationToken = cancellationTokenSource.Token }
-    let listening, server = startWebServerAsync defaultConfig (Successful.OK "")
-    Async.Start server
+    //let cancellationTokenSource = new CancellationTokenSource()
+    //let config = { defaultConfig with cancellationToken = cancellationTokenSource.Token }
+    //let listening, server = startWebServerAsync defaultConfig (Successful.OK "")
+    let scheduleJob = async {
+        let! res = scheduler.ScheduleJob(job, trigger)
+        res
+    }
+
+    Async.RunSynchronously scheduleJob
+    //Async.Start server
     Console.ReadKey true
         |> ignore
     printfn "Hello World from F#!"
